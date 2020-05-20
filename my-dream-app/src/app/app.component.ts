7b9a1923bddce3bd21762364a0fca421395e7875
kingvinay110 <kingvinay110@gmail.com>
2020-05-20 16:48:50 +0530

    intial commit

--- my-dream-app/src/app/app.component.ts
+++ my-dream-app/src/app/app.component.ts
@@ -3,20 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  name = 'my-first-app';
-  warning = false;
-  success = false;
-
-  OpenComponent(type){
-  if(type === 'Warning'){
-    this.warning = true;
-  }
-  else if(type === 'Success') {
-    this.success = true;
-  }
-  }
-  
 }
