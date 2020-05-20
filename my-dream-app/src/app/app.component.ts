e64a0e0b7c917b3dc6b16c4ecaa75e2519f9c592
kingvinay110 <kingvinay110@gmail.com>
2020-05-20 17:20:23 +0530

    Practice Databinding

--- my-dream-app/src/app/app.component.ts
+++ my-dream-app/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  DataBinding : FormGroup;
+  username="Vineeth";
+
+  ngOnInit() {
+    this.createForm();
+  }
+
+  createForm(){
+  this.DataBinding = new FormGroup({
+  UserName: new FormControl('')
+  });
+}
+
+  IsDisable() {
+    if(this.DataBinding.get('UserName').value.length === 0)
+    {
+      return true;
+    }
+  }
+
+  reset() {
+    this.username='';
+  }
 }
--- my-dream-app/src/app/app.module.ts
+++ my-dream-app/src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
