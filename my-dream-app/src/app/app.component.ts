44c37fa92d5797e54b80277f90b6e1445b8ab045
kingvinay110 <kingvinay110@gmail.com>
2020-07-12 15:56:41 +0530

    Start_project

--- my-dream-app/src/app/app.component.ts
+++ my-dream-app/src/app/app.component.ts
@@ -6,25 +6,4 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  name = "vineeth";
-  Display = false;
-  count =[];
-  click = 0;
-  limit = 5;
-
-  DisplayDetails() {
-    this.click++;
-    this.count.push(this.click);
-    this.Display = !this.Display;
-  }
-
-  setColor(i: number): string {
-    if (i >= this.limit) {
-      return 'blue';
-    } else {
-      return 'white';
-    }
-  }
-
-  
 }
